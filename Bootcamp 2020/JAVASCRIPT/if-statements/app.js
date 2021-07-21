be10a66271d14bff15fea4c6c8182c6e02743848
Moose17 <63780114+Mooose17@users.noreply.github.com>
2021-07-21 15:07:11 +0100

    example conditionals

---
+++ Bootcamp 2020/JAVASCRIPT/if-statements/app.js
@@ -0,0 +1,45 @@
+if(1+1===2){
+    console.log("MATH SKILL 1000")
+}
+
+let random = Math.random();
+if (random >= 0.5){
+    console.log("This random number is more than 5.")
+    console.log(random);
+} else {
+    console.log("This random number is less than 5.")
+    console.log(random);
+}
+
+
+const dayOfWeek = "Tuesday";
+
+if (dayOfWeek === "Monday"){
+    console.log("I HATE MONDAYS!!")
+} else if(dayOfWeek === "Tuesday"){
+    console.log("ugh!")
+} else if(dayOfWeek === "Wednesday"){
+    console.log("ugh!")
+} else if(dayOfWeek === "Thursday"){
+    console.log("ugh!")
+} else if(dayOfWeek === "Friday"){
+    console.log("YAY THE WEEKEND!")
+} else if(dayOfWeek === "Saturday"){
+    console.log("YAY THE WEEKEND!")
+} else if(dayOfWeek === "Sunday"){
+    console.log("YAY THE WEEKEND!")
+} else{
+    console.log("Unexpected result.")
+}
+
+const age = 77;
+if(age < 5){
+    console.log("You a baby. Free Entry")
+} else if(age < 10){
+    console.log("You are a child, $10 plz.")
+} else if(age < 65){
+    console.log("You are an adult. Pay $20")
+}
+else{
+    console.log("You are a Senior, $10.")
+}
